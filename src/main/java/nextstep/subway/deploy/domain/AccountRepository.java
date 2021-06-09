d1775484b39a8515642097a3e1c77f0745a576f2
김우재 <kwj1270@naver.com>
2021-06-09 14:04:08 +0900

    fix:test에@ActiveProfiles('test')추가

--- src/main/java/nextstep/subway/deploy/domain/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package nextstep.subway.deploy.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
