c9a992c8e23e15292b41f80bf987989a45e1f917
dev.prod(박동엽)/kakaopay <dev.prod@kakaopaycorp.com>
2021-06-11 07:35:20 +0900

    feat: DB 초기화 스크립트 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,49 @@
-create table account (
-    id   bigint AUTO_INCREMENT not null,
-    name varchar(255) not null,
+create table favorite
+(
+    id                bigint auto_increment,
+    created_date      timestamp,
+    modified_date     timestamp NULL DEFAULT NULL,
+    member_id         bigint,
+    source_station_id bigint,
+    target_station_id bigint,
+    primary key (id)
+);
+create table line
+(
+    id            bigint auto_increment,
+    created_date  timestamp,
+    modified_date timestamp NULL DEFAULT NULL,
+    color         varchar(255),
+    name          varchar(255),
+    primary key (id)
+);
+
+create table member
+(
+    id            bigint auto_increment,
+    created_date  timestamp,
+    modified_date timestamp NULL DEFAULT NULL,
+    age           integer,
+    email         varchar(255),
+    password      varchar(255),
+    primary key (id)
+);
+
+create table section
+(
+    id              bigint auto_increment,
+    distance        integer not null,
+    down_station_id bigint,
+    line_id         bigint,
+    up_station_id   bigint,
+    primary key (id)
+);
+
+create table station
+(
+    id            bigint auto_increment,
+    created_date  timestamp,
+    modified_date timestamp NULL DEFAULT NULL,
+    name          varchar(255),
     primary key (id)
 );
