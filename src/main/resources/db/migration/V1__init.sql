0063777f1296755068d49d1ab8337aea87881e05
dev.prod(박동엽)/kakaopay <dev.prod@kakaopaycorp.com>
2021-06-11 10:17:15 +0900

    feat: flyway sql 변경

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,18 +1,19 @@
 create table favorite
 (
-    id                bigint auto_increment,
-    created_date      timestamp,
-    modified_date     timestamp NULL DEFAULT NULL,
+    id                bigint not null auto_increment,
+    created_date      datetime,
+    modified_date     datetime NULL DEFAULT NULL,
     member_id         bigint,
     source_station_id bigint,
     target_station_id bigint,
     primary key (id)
 );
+
 create table line
 (
-    id            bigint auto_increment,
-    created_date  timestamp,
-    modified_date timestamp NULL DEFAULT NULL,
+    id            bigint not null auto_increment,
+    created_date  datetime,
+    modified_date datetime NULL DEFAULT NULL,
     color         varchar(255),
     name          varchar(255),
     primary key (id)
@@ -20,9 +21,9 @@ create table line
 
 create table member
 (
-    id            bigint auto_increment,
-    created_date  timestamp,
-    modified_date timestamp NULL DEFAULT NULL,
+    id            bigint not null auto_increment,
+    created_date  datetime,
+    modified_date datetime NULL DEFAULT NULL,
     age           integer,
     email         varchar(255),
     password      varchar(255),
@@ -31,7 +32,7 @@ create table member
 
 create table section
 (
-    id              bigint auto_increment,
+    id              bigint not null auto_increment,
     distance        integer not null,
     down_station_id bigint,
     line_id         bigint,
@@ -41,9 +42,9 @@ create table section
 
 create table station
 (
-    id            bigint auto_increment,
-    created_date  timestamp,
-    modified_date timestamp NULL DEFAULT NULL,
+    id            bigint not null auto_increment,
+    created_date  datetime,
+    modified_date datetime NULL DEFAULT NULL,
     name          varchar(255),
     primary key (id)
 );
