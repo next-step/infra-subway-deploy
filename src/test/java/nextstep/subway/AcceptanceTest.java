e73f1a741830665c9161e3c27888cc299d5da992
김석진 <7271kim@naver.com>
2021-06-19 13:06:51 +0900

    fix : [Step2] 2단계 - test 시 test profile 활성화

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -1,13 +1,16 @@
 package nextstep.subway;
 
-import io.restassured.RestAssured;
-import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.restassured.RestAssured;
+import nextstep.subway.utils.DatabaseCleanup;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AcceptanceTest {
     @LocalServerPort
     int port;
