6b07bbde6d26f4a37a9955ae1d0825b75ed9a2af
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-22 02:48:48 +0900

    refactor: 제너릭과 관련된 Sonarlint 코드 악취 제거

--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -19,7 +19,7 @@ public class MemberController {
     }
 
     @PostMapping("/members")
-    public ResponseEntity createMember(@RequestBody MemberRequest request) {
+    public ResponseEntity<Object> createMember(@RequestBody MemberRequest request) {
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
