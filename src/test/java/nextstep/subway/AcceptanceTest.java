af694b3140f981912fa1d438c09061e519f1a55e
이준형 <p_illusion@naver.com>
2021-07-07 09:25:53 +0900

    Step2 (#129)
    * doc: step2 미션 적용
    
    * refactor: 서브모듈 기능을 사용하여 참조하도록 수정
    
    * refactor: poroperties 적용
    
    * refactor: poroperties 적용
    
    * refactor: flyway 적용
    
    * refactor: 사용하지 않는 property 제거
    
    * refactor: poroperties 복구
    
    * refactor: TLS 적용 주소 변경

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AcceptanceTest {
     @LocalServerPort
     int port;
