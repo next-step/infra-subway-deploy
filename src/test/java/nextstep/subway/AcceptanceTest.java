9e09a8a35ede9c34eaeec673763deffa4d8197da
Byeonggil Park <bgpark82@gmail.com>
2021-11-27 19:25:23 +0900

    2단계 - 서비스 배포하기 (#160)
    * chore: application-local.properties 무시
    
    * chore: mysql connection 추가
    
    * chore: flyway 설정 추가
    
    * chore: 초기 테이블 설정
    
    * fix: 초기 테이블 삭제
    
    * chore: 마이그레이션 설정 추가
    
    * chore: 테스트 설정 추가
    
    * chore: 불필요한 코드 삭제
    
    * doc: README 추가
    
    Co-authored-by: peter-alethio <peter@alethio.io>

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
     @LocalServerPort
