bdebd00af3aac93be6cb33b6aebba6b312a091cb
Jeongsu Yang <mycat83@gmail.com>
2021-11-28 00:05:06 +0900

    feat: flyway 설정 추가

---
+++ src/main/java/nextstep/subway/account/Account.java
@@ -0,0 +1,30 @@
+package nextstep.subway.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Account() {
+    }
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
