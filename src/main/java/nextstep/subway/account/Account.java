649ac84e44b6bd7cadfbf9b381bde76fa1404b08
wangkdk <31848426+wangkdk@users.noreply.github.com>
2021-11-28 19:58:06 +0900

    2단계 - 서비스 배포하기 (#165)
    * docs: tls 적용 URL
    
    * feature: properties 설정 분리
    
    - 서브모듈 활용
    
    * chore: submodule update
    
    * fix: test 구동시 test 설정파일 사용하도록 수정
    
    * feature: flyway 적용
    
    * style: 공백제거
    
    * chore: 접속정보 변경
    
    * chore: 설정파일 변경
    
    * chore: 설정파일 분리 이력
    
    * chore: 설정파일 삭제
    
    * feature: flyway 실습
    
    * fix: account age 추가

---
+++ src/main/java/nextstep/subway/account/Account.java
@@ -0,0 +1,17 @@
+package nextstep.subway.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int age;
+}
