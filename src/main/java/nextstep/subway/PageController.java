dffdc4efa7466d2847c72ad49d1f17a3892fd05b
lewisseo91 <togokr@naver.com>
2021-11-29 18:56:21 +0900

    feat(PageController): logger 추가
    - logger 추가

--- src/main/java/nextstep/subway/PageController.java
+++ src/main/java/nextstep/subway/PageController.java
@@ -1,11 +1,15 @@
 package nextstep.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    private static final Logger log = LoggerFactory.getLogger(PageController.class);
+
     private Properties properties;
 
     public PageController(Properties properties) {
@@ -26,7 +30,7 @@ public class PageController {
     public String index() {
         String name = properties.getName();
         String password = properties.getPassword();
-        System.out.printf("user name : %s, password : %s\n", name, password);
+        log.info("user name : {}, password : {}", name, password);
         return "checkIndex : " + name;
     }
 }
