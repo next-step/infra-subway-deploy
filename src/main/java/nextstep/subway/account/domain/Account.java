5757a0ecd2fe1818d0c276277d50476e43deb1e0
Choi hong hee <vvshinevv@gmail.com>
2021-12-02 23:32:20 +0900

    🚀 2단계 - 서비스 배포하기 (#203)
    * feat: 환경별로 설정 파일 나누기
    
    * feat: flyway 설정 추가
    
    * feat: flyway v1 적용
    
    * fix : prod환경 db url 수정
    
    * test: 테스트 환경 분리
    
    * bug: prod환경 포트 수정
    
    * feat : flyway version2
    
    Co-authored-by: Honghee Choi <vvshinevv@nhnsoft.com>

---
+++ src/main/java/nextstep/subway/account/domain/Account.java
@@ -0,0 +1,34 @@
+package nextstep.subway.account.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+    public Account() {
+    }
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
