2406fa7d466db6a4688ce9d67d6f1f015403569a
이채윤 <hopefulyun@gmail.com>
2021-12-07 22:55:28 +0900

    Step2 리뷰 요청드립니다 (#228)
    * chore: flyway, mysql docker 적용
    
    * feat: 리버스 프록시 HTTPS 적용

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -1,27 +1,30 @@
 package nextstep.subway;
 
-import io.restassured.RestAssured;
-import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+import io.restassured.RestAssured;
+import nextstep.subway.utils.DatabaseCleanup;
+
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
-    @LocalServerPort
-    int port;
+	@LocalServerPort
+	int port;
 
-    @Autowired
-    private DatabaseCleanup databaseCleanup;
+	@Autowired
+	private DatabaseCleanup databaseCleanup;
 
-    @BeforeEach
-    public void setUp() {
-        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-            RestAssured.port = port;
-            databaseCleanup.afterPropertiesSet();
-        }
+	@BeforeEach
+	public void setUp() {
+		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+			RestAssured.port = port;
+			databaseCleanup.afterPropertiesSet();
+		}
 
-        databaseCleanup.execute();
-    }
+		databaseCleanup.execute();
+	}
 }
