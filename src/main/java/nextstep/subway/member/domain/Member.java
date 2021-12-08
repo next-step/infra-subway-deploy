04587b06aaa5529ab91f3d87b3f1b128387948a5
Chloe <page.prologue@gmail.com>
2021-12-08 23:11:42 +0900

    feat : local 환경에서 flyway 적용

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -14,6 +14,7 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private String email;
     private String password;
     private Integer age;
---
+++ src/main/resources/db/migration/V2__add_name_in_member.sql
@@ -0,0 +1,1 @@
+ALTER TABLE member ADD COLUMN name varchar(50);
