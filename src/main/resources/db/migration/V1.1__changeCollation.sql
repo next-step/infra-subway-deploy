f517ace283928e384da45330de9730d2845efc1d
minho <lights8615@gmail.com>
2021-12-11 13:19:01 +0900

    feat(step2): 지하철 역 애플리케이션의 스키마를 관리하도록 flyway 개선

---
+++ src/main/resources/db/migration/V1.1__changeCollation.sql
@@ -0,0 +1,1 @@
+ALTER database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/main/resources/db/migration/V1.2__changeTableCollation.sql
@@ -0,0 +1,3 @@
+ALTER table line CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER table station CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
