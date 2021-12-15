abd007ddae1bdee876482c1febb694f432a119a9
daehyun0 <dd70296@gmail.com>
2021-12-16 05:19:08 +0900

    fix: flyway migration script V2 semicolon 추가

--- src/main/resources/db/migration/V2__add_line.sql
+++ src/main/resources/db/migration/V2__add_line.sql
@@ -1,2 +1,2 @@
 INSERT INTO line(created_date, modified_date, color, name)
-VALUES ('2021-12-16T00:00:00', '2021-12-16T00:00:00', 'bg-red-600', '새로추가된노선')
+VALUES ('2021-12-16T00:00:00', '2021-12-16T00:00:00', 'bg-red-600', '새로추가된노선');
