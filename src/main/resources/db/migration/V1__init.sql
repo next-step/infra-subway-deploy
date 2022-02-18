7fdfc7a3ae2c2b36efbb34828475b4d67143d0e3
mac_daehwan2yo <daehwan7989@naver.com>
2022-02-18 22:40:54 +0900

    add : V1__init.sql 추가

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+create table member (
+                        id bigint auto_increment,
+                        name varchar(255) ,
+                        email varchar ,
+                        password varchar ,
+                        age int,
+                        primary key (id)
+)
