89d5f0f39cc5b7cfedeabe42708dddb12223ac95
joel-you <ruew12@gmail.com>
2022-03-19 04:38:07 +0900

    docs(step2): 2단계 미션 수행
    * flyway build 버그 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,59 @@
-DROP TABLE IF EXISTS `member`;
+drop table if exists `section`;
+drop table if exists `member`;
+drop table if exists `favorite`;
+drop table if exists `line`;
+drop table if exists `station`;
 
 CREATE TABLE `member` (
-                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                          `created_date` datetime(6) DEFAULT NULL,
-                          `modified_date` datetime(6) DEFAULT NULL,
-                          `age` int(11) DEFAULT NULL,
-                          `email` varchar(255) DEFAULT NULL,
-                          `password` varchar(255) DEFAULT NULL,
-                          PRIMARY KEY (`id`)
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime(6) DEFAULT NULL,
+  `modified_date` datetime(6) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `member` (`created_date`, `modified_date`, `age`, `email`, `password`)
-VALUES('220319', '220319', 35, 'admin@gmail.com', '1234');
+CREATE TABLE `favorite` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `created_date` datetime(6) DEFAULT NULL,
+    `modified_date` datetime(6) DEFAULT NULL,
+    `member_id` bigint(20) DEFAULT NULL,
+    `source_station_id` bigint(20) DEFAULT NULL,
+    `target_station_id` bigint(20) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `line` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`created_date` datetime(6) DEFAULT NULL,
+`modified_date` datetime(6) DEFAULT NULL,
+`color` varchar(255) DEFAULT NULL,
+`name` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `UK_9ney9davbulf79nmn9vg6k7tn` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `station` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `created_date` datetime(6) DEFAULT NULL,
+   `modified_date` datetime(6) DEFAULT NULL,
+   `name` varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `UK_gnneuc0peq2qi08yftdjhy7ok` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=728 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `section` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `distance` int(11) NOT NULL,
+   `down_station_id` bigint(20) DEFAULT NULL,
+   `line_id` bigint(20) DEFAULT NULL,
+   `up_station_id` bigint(20) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `FKtecjgrtoxbeeqpymapva62xfw` (`down_station_id`),
+   KEY `FKlfhpg8lrvyr948juittt221ux` (`line_id`),
+   KEY `FK18bw17tb86fh1igov96s9i6he` (`up_station_id`),
+   CONSTRAINT `FK18bw17tb86fh1igov96s9i6he` FOREIGN KEY (`up_station_id`) REFERENCES `station` (`id`),
+   CONSTRAINT `FKlfhpg8lrvyr948juittt221ux` FOREIGN KEY (`line_id`) REFERENCES `line` (`id`),
+   CONSTRAINT `FKtecjgrtoxbeeqpymapva62xfw` FOREIGN KEY (`down_station_id`) REFERENCES `station` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=341 DEFAULT CHARSET=utf8mb4;
