1d66fe521bc57e2f5cd27c58ae292429fad90646
ka0oll <60064643+ka0oll@users.noreply.github.com>
2022-03-20 15:11:45 +0900

    2단계 - 배포하기  (#332)

--- src/test/java/nextstep/subway/utils/DatabaseCleanup.java
+++ src/test/java/nextstep/subway/utils/DatabaseCleanup.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@ActiveProfiles("test")
 public class DatabaseCleanup implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
