0053d26d79b2099d8d8b7dd64216a9f735c756ff
Sung-jin <osj4872@gmail.com>
2022-06-03 17:09:44 +0900

    chore: application + ssl 을 docker-compose 로 동작하도록 docker 설정 및 실행 스크립트 작성

---
+++ docker/start.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ "$(docker ps -q)" ]; then
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+fi
+
+DOCKER_PATH=$(dirname "$0")
+
+if [ -z "$1" ]
+  then echo "도메인 정보가 필요합니다"
+  exit
+fi
+
+if [ -z "$2" ]
+  then echo "이메일 정보가 필요합니다."
+  exit
+fi
+
+if [ -z "$3" ]
+  then echo "앱 실행 환경이 필요합니다"
+  exit
+fi
+
+sed -i "s/{DOMAIN}/$1/g" $DOCKER_PATH/docker-compose.yml
+sed -i "s/{EMAIL}/$2/g" $DOCKER_PATH/docker-compose.yml
+sed -i "s/{ENV}/$3/g" $DOCKER_PATH/Dockerfile
+
+cd "$DOCKER_PATH"/../
+./gradlew build
+cp build/libs/*.jar docker/application.jar
+
+cd docker
+ls -al
+docker-compose up -d
