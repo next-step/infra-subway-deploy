880ca1f9ef6820d0823b67c7aea03b87737ea62a
tasklet1579 <tasklet1579@gmail.com>
2022-06-04 23:04:26 +0900

    test : 테스트 프로필 활성화

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -3,10 +3,14 @@ package nextstep.subway;
 import io.restassured.RestAssured;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
     @LocalServerPort
@@ -23,5 +27,10 @@ public class AcceptanceTest {
         }
 
         databaseCleanup.execute();
+    }
+
+    @DisplayName("test profile을 활성화 한다")
+    @Test
+    void activeProfile() {
     }
 }
