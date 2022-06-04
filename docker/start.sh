78f1ab9dd4ec063de881c0dc08e9bb2a674d7d84
Sung-jin <osj4872@gmail.com>
2022-06-04 21:38:55 +0900

    chore: local 환경 구성
    docker 로 mysql, application 연동하여 동작하도록 구성
    실행 스크립트를 local, prod 에서만 동작하도록 수정

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DOCKER_PATH=$(dirname "$0")
+
 if [ "$(docker ps -q)" ]; then
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
@@ -9,26 +11,32 @@ if [ "$(docker images 'subway' -a -q)" ]; then
     docker rmi $(docker images 'subway' -a -q)
 fi
 
-DOCKER_PATH=$(dirname "$0")
+case "$1" in
+  "prod") executeProd ;;
+  "local") ;;
+  *)
+    echo "앱 실행 환경은 prod 또는 local 만 가능합니다."
+    exit
+esac
 
-if [ -z "$1" ]
-  then echo "도메인 정보가 필요합니다"
-  exit
-fi
+function executeProd() {
+  if [ -z "$2" ]
+    then echo "도메인 정보가 필요합니다"
+    exit
+  fi
 
-if [ -z "$2" ]
-  then echo "이메일 정보가 필요합니다."
-  exit
-fi
+  if [ -z "$3" ]
+    then echo "이메일 정보가 필요합니다."
+    exit
+  fi
+}
 
-if [ -z "$3" ]
-  then echo "앱 실행 환경이 필요합니다"
-  exit
-fi
+cp $DOCKER_PATH/template/docker-compose-"$1".yml $DOCKER_PATH/docker-compose.yml
+cp $DOCKER_PATH/template/Dockerfile $DOCKER_PATH/Dockerfile
 
-sed -i "s/{DOMAIN}/$1/g" $DOCKER_PATH/docker-compose.yml
-sed -i "s/{EMAIL}/$2/g" $DOCKER_PATH/docker-compose.yml
-sed -i "s/{ENV}/$3/g" $DOCKER_PATH/Dockerfile
+sed -i "" "s/{ENV}/$1/g" $DOCKER_PATH/Dockerfile
+sed -i "" "s/{DOMAIN}/$2/g" $DOCKER_PATH/docker-compose.yml
+sed -i "" "s/{EMAIL}/$3/g" $DOCKER_PATH/docker-compose.yml
 
 cd "$DOCKER_PATH"/../
 SPRING_PROFILE_ACTIVE=test ./gradlew clean build
