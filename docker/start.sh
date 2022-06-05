2a39e95db94c552318f5ee44cb34848669655929
Sung-jin <osj4872@gmail.com>
2022-06-05 18:39:53 +0900

    fix: 실행 스크립트 버그 수정
    함수가 사용하는 시점 이후에 선언되어 있어서 실행 안되는 현상 수정
    스크립트 argument 가 사용하는 시점에 호출이 안되는 현상 수정

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 DOCKER_PATH=$(dirname "$0")
+ENV=$1
+DOMAIN=$2
+EMAIL=$3
 
 if [ "$(docker ps -q)" ]; then
     docker stop $(docker ps -a -q)
@@ -11,7 +14,19 @@ if [ "$(docker images 'subway' -a -q)" ]; then
     docker rmi $(docker images 'subway' -a -q)
 fi
 
-case "$1" in
+function executeProd() {
+  if [ -z "$DOMAIN" ]
+    then echo "도메인 정보가 필요합니다"
+    exit
+  fi
+
+  if [ -z "$EMAIL" ]
+    then echo "이메일 정보가 필요합니다."
+    exit
+  fi
+}
+
+case "$ENV" in
   "prod") executeProd ;;
   "local") ;;
   *)
@@ -19,24 +34,12 @@ case "$1" in
     exit
 esac
 
-function executeProd() {
-  if [ -z "$2" ]
-    then echo "도메인 정보가 필요합니다"
-    exit
-  fi
-
-  if [ -z "$3" ]
-    then echo "이메일 정보가 필요합니다."
-    exit
-  fi
-}
-
 cp $DOCKER_PATH/template/docker-compose-"$1".yml $DOCKER_PATH/docker-compose.yml
 cp $DOCKER_PATH/template/Dockerfile $DOCKER_PATH/Dockerfile
 
-sed -i "" "s/{ENV}/$1/g" $DOCKER_PATH/Dockerfile
-sed -i "" "s/{DOMAIN}/$2/g" $DOCKER_PATH/docker-compose.yml
-sed -i "" "s/{EMAIL}/$3/g" $DOCKER_PATH/docker-compose.yml
+sed -i "s/{ENV}/$ENV/g" $DOCKER_PATH/Dockerfile
+sed -i "s/{DOMAIN}/$DOMAIN/g" $DOCKER_PATH/docker-compose.yml
+sed -i "s/{EMAIL}/$EMAIL/g" $DOCKER_PATH/docker-compose.yml
 
 cd "$DOCKER_PATH"/../
 SPRING_PROFILE_ACTIVE=test ./gradlew clean build
