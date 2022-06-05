5e0b51d07a9b00702c46d34f911077772d464622
gunan <power4889@gmail.com>
2022-06-06 02:02:31 +0900

    refactor: 서버 실행 스크립트 분리

--- deploy.sh
+++ deploy.sh
@@ -51,17 +51,5 @@ function pull() {
 }
 pull;
 
-## gradle build
-$SHELL_SCRIPT_PATH/gradlew clean build
-
-## 프로세스 pid를 찾는 명령어
-PID=`pgrep -f $SHELL_SCRIPT_PATH/build/libs/*.jar`
-
-## 프로세스를 종료하는 명령어
-if [[ -n "$PID" ]]; then
-  kill -9 $PID
-fi
-
-## 프로세스를 실행!
-JARFILE=`find $SHELL_SCRIPT_PATH/build/libs/* -name "*jar"`
-nohup java -jar -Dspring.profiles.active=$PROFILE $JARFILE 1> ~/subway.log 2>&1 &
+## 실행
+bash $SHELL_SCRIPT_PATH/startup.sh $PROFILE
