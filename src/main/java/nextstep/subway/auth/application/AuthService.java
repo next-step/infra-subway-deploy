03d65f50e0d689247d8bef3d2a72728f9af3813e
jhsong2580 <thdwmdgns@naver.com>
2022-06-06 22:54:54 +0900

    docs

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -35,7 +35,7 @@ public class AuthService {
 
         String email = jwtTokenProvider.getPayload(credentials);
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
-        /* 배포 테스트1 */
+        /* 배포 테스트2 */
         return new LoginMember(member.getId(), member.getEmail(), member.getAge());
     }
 }
