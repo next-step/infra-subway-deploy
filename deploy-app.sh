33a2ecf5a0a757b191914f34baf5d3fbdad7fb5c
Ubuntu <suyounlake@naver.com>
2022-06-11 14:38:33 +0000

    config: 배포 스크립트 작성

---
+++ deploy-app.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+## 변수 설정
+
+txtrst='\033[1;37m' # White
+txtred='\033[1;31m' # Red
+txtylw='\033[1;33m' # Yellow
+txtpur='\033[1;35m' # Purple
+txtgrn='\033[1;32m' # Green
+txtgra='\033[1;30m' # Gray
+
+
+echo -e "${txtylw}=======================================${txtrst}"
+echo -e "${txtgrn}  << 스크립트 🧐 >>${txtrst}"
+echo -e "${txtylw}=======================================${txtrst}"
+
+BRANCH=$1
+PROFILE=$2
+
+## 저장소 pull
+function pull() {
+  echo -e ""
+  echo -e ">> Pull Request 🏃♂️ "
+  git pull origin ${BRANCH}
+}
+
+## gradle build
+function build() {
+  echo -e ""
+  echo -e ">> Gradle Build 🏃♂️ "
+  ./gradlew clean build
+}
+
+## 프로세스 pid를 찾아저 종료
+function killApp() {
+  process=$(ps -ef | grep 'subway')
+  pid=$(echo ${process} | cut -d " " -f2)
+  if [ -n "${pid}" ]
+  then
+      result1=$(kill -9 ${pid})
+      echo -e ">> Process is killed."
+  else
+      echo -e ">> Running process not found."
+  fi
+}
+
+## 애플리케이션 실행
+function startApp() {
+  app=$(find ./* -name "*jar")
+  if [ -n "${app}" ]
+  then
+      nohup java -jar -Dspring.profiles.active=${PROFILE} ${app} 1> /home/ubuntu/nextstep/log/application.log 2>&1  &
+      echo -e ">> Start Application"
+  else
+      echo -e ">> jar file is not found."
+  fi
+}
+
+
+pull
+build
+killApp
+startApp
+echo -e""
+echo -e ">> Application Deploy Finished 🏃♂️ "
+
