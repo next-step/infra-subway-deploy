486133c27faeff5d108b59e59012b3cb1aae965a
Giwan Kim <giwankim@gmail.com>
2022-06-25 07:13:16 +0900

    fix: add line separators to script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,48 +29,61 @@ fi
 
 function pull() {
   echo -e ""
+  echo -e "${txtylw}=======================================${txtrst}"
   echo -e ">> Pull Request 🏃"
   git pull origin $BRANCH
+  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 function build() {
   echo -e ""
+  echo -e "${txtylw}=======================================${txtrst}"
   echo -e ">> Gradle clean build 🏃"
   ./gradlew clean build
   echo -e ""
   echo -e ">> Build 파일 복사 🏃"
   cp $REPOSITORY/build/libs/*.jar $REPOSITORY
+  echo -e "${txtylw}=======================================${txtrst}"
 }
 
-function stop() {
+function kill() {
   echo -e ""
+  echo -e "${txtylw}=======================================${txtrst}"
   echo -e ">> 현재 구동중인 애플리케이션 pid 확인 🏃"
   CURRENT_PID=$(pgrep -f subway*.jar)
   echo -e "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
-  if [ -z "$CURRENT_PID"]; then
+  if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
   else
     echo "> kill -2 $CURRENT_PID"
     kill -2 $CURRENT_PID
     sleep 5
   fi
+  echo -e "${txtylw}=======================================${txtrst}"
+
 }
 
 function start() {
   echo -e ""
+  echo -e "${txtylw}=======================================${txtrst}"
   echo -e ">> 새 애플리케이션 배포 🏃"
   JARFILE=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
   nohup java -jar -Dspring.profiles.active=$PROFILE $JARFILE 1> $REPOSITORY/logs/$PROFILE 2>&1 &
+  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 ## 프로젝트 디렉토리로 이동
 cd $REPOSITORY
+
 ## 저장소 pull
 pull;
+
 ## gradle build
 build;
+
 # 프로세스 pid를 찾고 종료하는 명령어
-stop;
+kill;
+
 # 프로세스를 시작하는 명령어
 start;
