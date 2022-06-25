8faccb817f705a90b38e8c849ad1de5019cfe2d0
Giwan Kim <giwankim@gmail.com>
2022-06-25 09:44:27 +0900

    fix: deploy remove yellow lines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,24 +28,22 @@ then
 fi
 
 function pull() {
-  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ""
   echo -e ">> Pull Request 🏃"
   git pull origin $BRANCH
-  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 function build() {
-  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ""
   echo -e ">> Gradle clean build 🏃"
   ./gradlew clean build
   echo -e ""
   echo -e ">> Build 파일 복사 🏃"
   cp $REPOSITORY/build/libs/*.jar $REPOSITORY
-  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 function kill() {
-  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ""
   echo -e ">> 현재 구동중인 애플리케이션 pid 확인 🏃"
   CURRENT_PID=$(pgrep -f subway*.jar)
   echo -e "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
@@ -57,17 +55,15 @@ function kill() {
     kill -2 $CURRENT_PID
     sleep 5
   fi
-  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 function start() {
-  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ""
   echo -e ">> 새 애플리케이션 배포 🏃"
   JARFILE=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
   echo -e ${JARFILE}
   echo -e "-Dspring.profiles.active=${PROFILE}"
   nohup java -jar -Dspring.profiles.active=$PROFILE $JARFILE 1> $REPOSITORY/logs/$PROFILE 2>&1 &
-  echo -e "${txtylw}=======================================${txtrst}"
 }
 
 ## 프로젝트 디렉토리로 이동
