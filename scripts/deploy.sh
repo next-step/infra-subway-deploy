648a93e0ca675227cdbae60c80d3557296872a30
Giwan Kim <giwankim@gmail.com>
2022-06-25 15:10:19 +0900

    fix: kill with -15

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -51,8 +51,8 @@ function kill() {
   if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
   else
-    echo "> kill -2 $CURRENT_PID"
-    kill -2 $CURRENT_PID
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
   fi
   echo -e "${txtylw}=======================================${txtrst}"
