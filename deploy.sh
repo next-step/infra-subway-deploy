d6fff800cbd83f5541075e273ebf4f3561412dc6
mukeunzi <eunji057@gmail.com>
2022-06-26 15:21:05 +0900

    refactor: 사용하지 않는 변수 삭제

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,7 @@ PROFILE=$2
 PROJECT_NAME=subway
 
 txtrst='\033[1;37m' # White
-txtred='\033[1;31m' # Red
 txtylw='\033[1;33m' # Yellow
-txtpur='\033[1;35m' # Purple
-txtgrn='\033[1;32m' # Green
-txtgra='\033[1;30m' # Gray
 
 ## 저장소 pull
 function pull() {
