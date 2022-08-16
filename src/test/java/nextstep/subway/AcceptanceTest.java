28ae349b5cc57fb31d66bb5478da2b996d281183
Dion <ksundong@gmail.com>
2022-08-17 01:07:34 +0900

    chore: 테스트용 데이터베이스 설정 추가 및 테스트 프로필 활성화

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AcceptanceTest {
     @LocalServerPort
     int port;
