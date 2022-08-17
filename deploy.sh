df196309180f9b19b23e313a8497b44bca43af63
Dion <ksundong@gmail.com>
2022-08-18 01:33:38 +0900

    chore: 배포 스크립트 함수 실행 여부에 따라 동작을 달리하도록 설정

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,24 @@ txtpur='\033[1;35m' # Purple
 txtgrn='\033[1;32m' # Green
 txtgra='\033[1;30m' # Gray
 
-echo -e "${txtylw}=======================================${txtrst}"
-echo -e "${txtgrn}        << 배포 스크립트 🧐 >>${txtrst}"
-echo -e "${txtylw}=======================================${txtrst}"
-echo -e "${txtpur} 1. repositoryPull         | 저장소 pull ${txtrst}"
-echo -e "${txtpur} 2. gradleBuild            | 배포 버전 빌드 ${txtrst}"
-echo -e "${txtpur} 3. getPid                 | 이전 배포 항목 PID 조회 ${txtrst}"
-echo -e "${txtpur} 4. killProcess            | PID 로 종료 ${txtrst}"
-echo -e "${txtpur} 5. checkProcessTerminated | 프로세스 종료 확인 ${txtrst}"
-echo -e "${txtpur} 6. nohupRun               | 백그라운드에서 빌드된 항목 배포 ${txtrst}"
-echo -e "${txtylw}=======================================${txtrst}"
+# function 실행인지 확인 (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e "${txtgrn}        << 배포 스크립트 🧐 >>${txtrst}"
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e "${txtpur} 1. repositoryPull         | 저장소 pull ${txtrst}"
+  echo -e "${txtpur} 2. gradleBuild            | 배포 버전 빌드 ${txtrst}"
+  echo -e "${txtpur} 3. getPid                 | 이전 배포 항목 PID 조회 ${txtrst}"
+  echo -e "${txtpur} 4. killProcess            | PID 로 종료 ${txtrst}"
+  echo -e "${txtpur} 5. checkProcessTerminated | 프로세스 종료 확인 ${txtrst}"
+  echo -e "${txtpur} 6. nohupRun               | 백그라운드에서 빌드된 항목 배포 ${txtrst}"
+  echo -e "${txtylw}=======================================${txtrst}"
+  exit 0
+fi
 
 function repositoryPull() {
     echo -e "${txtgrn} 저장소 pull 시작... 📥${txtrst}"
