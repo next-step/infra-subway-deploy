db2c29466505f3255785018356b26246e49d9a72
Sangmin <sm9171@nate.com>
2022-08-20 23:18:20 +0900

    merge

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-
-## 변수 설정
-BRANCH=$1
-PROFILE=$2
-
-=======
 ## 텍스트 변수 설정
->>>>>>> 9b67a42 (feature: step3 구현)
 txtrst='\033[1;37m' # White
 txtred='\033[1;31m' # Red
 txtylw='\033[1;33m' # Yellow
@@ -15,61 +7,6 @@ txtpur='\033[1;35m' # Purple
 txtgrn='\033[1;32m' # Green
 txtgra='\033[1;30m' # Gray
 
-<<<<<<< HEAD
-
-echo -e "${txtylw}=======================================${txtrst}"
-echo -e "${txtgrn}  << DEPLOY >>${txtrst}"
-echo -e "${txtylw}=======================================${txtrst}"
-
-function pull() {
-  echo -e ">> Getting branch 🏃♂️ "
-  git pull origin "$BRANCH"
-}
-
-function build() {
-  echo -e ">> Building 🏃..."
-  ./gradlew clean build -x test
-}
-
-function get_pid() {
-  echo -e "$(jps | grep "subway" | awk '{print $1}')"
-}
-
-function kill_app() {
-  local pid="$1"
-  if [[ -z $pid ]]; then
-    echo -e ">> PID not found 🏃..."
-  else
-    echo -e ">> Killing PID (PID: $pid) 🏃..."
-    kill "$pid"
-  fi
-}
-
-function start_app() {
-  echo -e ">> Starting up (Profile: $PROFILE) 🏃... "
-  nohup java -jar \
-        -Dspring.profiles.active="$PROFILE" \
-        $(find ./* -name "*.jar" | head -n 1) \
-        1>/home/ubuntu/subway.log \
-        2>&1 \
-        &
-}
-
-## 저장소 pull
-pull;
-
-## gradle build
-build;
-
-## 프로세스 pid를 찾는 명령어
-PID="$(get_pid)";
-
-## 프로세스를 종료하는 명령어
-kill_app "$PID";
-
-## 프로젝트 실행
-start_app;
-=======
 ## 스크림트 실행시 전달되는 파라미터
 
 EXECUTION_PATH=${pwd}
@@ -107,7 +44,7 @@ function repository_pull() {
     echo -e "${txtgrn} 저장소 pull 완료! ${txtrst}"
 }
 function gradle_build() {
-    echo -e "${txtgrn} gradle build 시작... 🐘${txtrst}"
+    echo -e "${txtgrn} gradle build 시작... ${txtrst}"
     "$SHELL_SCRIPT_PATH"/gradlew clean build
     echo -e "${txtgrn} gradle build 완료! ${txtrst}"
 }
@@ -144,4 +81,3 @@ get_pid
 kill_process
 get_pid
 nohup_run
->>>>>>> 9b67a42 (feature: step3 구현)
