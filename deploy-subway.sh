1c7ab8b9ce92bc6153d1a1dd05c62e66014b38b1
YINAKIM <yinaofficeg@gmail.com>
2022-08-22 01:10:04 +0900

    feat : week1-step3 ) 배포스크립트 작성 2 : 매개변수로 jar파일명 받아와서 실행 테스트

--- deploy-subway.sh
+++ deploy-subway.sh
@@ -27,4 +27,11 @@ echo -e ">> gradle clean build"
 ./gradlew clean build
 
 echo -e ">> find jar name"
-find ./* -name "*jar"
+JAR_FILE_NAME=$(find ./* -name "*jar")
+
+ACTIVE_PROFILE=prod
+echo ">> 배포할 파일명  ${JAR_NAME}"
+echo ">> 실행할 profile = ${ACTIVE_PROFILE}"
+
+## 실행하기
+nohup java -jar -Dspring.profiles.active=${ACTIVE_PROFILE} ${JAR_FILE_NAME} &
