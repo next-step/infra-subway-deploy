c930929c4affc670c460ae382d0654ed92a286e3
teachd000000 <teachd000000@gmail.com>
2022-09-25 02:32:51 +0900

    chore: 배포 스크립트 추가

---
+++ check-diff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANCH=$1
+
+git fetch
+
+MASTER=$(git rev-parse $BRANCH)
+REMOTE=$(git rev-parse origin/$BRANCH)
+
+if [ $MASTER = $REMOTE ]; then
+        exit 1
+fi
+
+echo -e "[$(date)] A change has been detected."
+git pull origin $BRANCH
+git checkout $BRANCH
+
+./gradlew clean build
+/bin/bash subway-service.sh
