3cd87b992c64b84abf5dd32448a98af4b6748d25
chunhodong <yhms4432@naver.com>
2022-11-23 12:48:31 +0900

    docs: 배포쉘스크립트파일추가

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## 변수 설정
+
+txtrst='\033[1;37m' # White
+txtred='\033[1;31m' # Red
+txtylw='\033[1;33m' # Yellow
+txtpur='\033[1;35m' # Purple
+txtgrn='\033[1;32m' # Green
+txtgra='\033[1;30m' # Gray
+
+
+echo -e "${txtylw}=======================================${txtrst}"
+echo -e "${txtgrn}  << 스크립트 🧐 >>${txtrst}"
+echo -e "${txtylw}=======================================${txtrst}"
