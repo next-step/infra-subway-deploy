f96dcffa2e2d3113dbb2c46c43400eb6871cb18d
chunhodong <yhms4432@naver.com>
2022-11-23 14:23:00 +0900

    docs: deploy.sh 배포스크립트작성

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,61 @@
 #!/bin/bash
 
-## 변수 설정
+## ...
+BRANCH=$1
 
-txtrst='\033[1;37m' # White
-txtred='\033[1;31m' # Red
-txtylw='\033[1;33m' # Yellow
-txtpur='\033[1;35m' # Purple
-txtgrn='\033[1;32m' # Green
-txtgra='\033[1;30m' # Gray
+function check_df() {
+  git fetch
+  master=$(git rev-parse $BRANCH)
+  remote=$(git rev-parse origin $BRANCH)
 
+  if [[ $master == $remote ]]; then
+    echo -e "[$(date)] Nothing to do!!! 😫"
+    exit 0
+  else
+    do_deploy
+  fi
+}
 
-echo -e "${txtylw}=======================================${txtrst}"
-echo -e "${txtgrn}  << 스크립트 🧐 >>${txtrst}"
-echo -e "${txtylw}=======================================${txtrst}"
+function do_deploy() {
+  echo -e "${txtgrn}>> do deploy ${txtrst}"
+  git_pull
+  gradle_build
+  find_process
+  kill_process
+  start_process
+}
+
+function git_pull() {
+  echo -e "${txtgrn}>> git pull ${txtrst}"
+  git pull origin master
+}
+
+function gradle_build() {
+  echo -e "${txtgrn}>> gradle build ${txtrst}"
+  git pull origin master
+}
+
+function find_process() {
+  echo -e "${txtgrn}>> find process ${txtrst}"
+  git pull origin master
+}
+
+function kill_process() {
+  echo -e "${txtgrn}>> kill process ${txtrst}"
+  git pull origin master
+}
+
+function start_process() {
+  echo -e "${txtgrn}>> start process ${txtrst}"
+  nohup java -jar -Dspring.profiles.active=prod ./build/libs/subway-0.0.1-SNAPSHOT.jar 1> subway.log 2>&1 &
+}
+
+function start_deploy() {
+    echo -e "${txtgrn}=======================================${txtrst}"
+    echo -e "${txtgrn}  >> deploy start ${txtrst}"
+    check_df
+    echo -e "${txtgrn}  >> deploy finish ${txtrst}"
+    echo -e "${txtgrn}=======================================${txtrst}"
+}
+
+start_deploy;
