d2b5785e7ee272b5fb9c9636580c6322f7a96ce1
Haseop Lee <haservi@naver.com>
2022-11-25 23:37:26 +0900

    test(Script): 변경 스크립트 확인

--- scripts/check_df.sh
+++ scripts/check_df.sh
@@ -2,9 +2,10 @@ git fetch
 BRANCH=step3
 
 master=$(git rev-parse $BRANCH)
-remote=$(git rev-parse origin $BRANCH)
+remote=$(git rev-parse origin/$BRANCH)
 
 if [[ $master == $remote ]]; then
   echo -e "[$(date)] 변경된 내용이 없습니다. 😫"
   exit 0
 fi
+echo -e "[$(date)] 업데이트 내용이 있습니다. 🤩"
