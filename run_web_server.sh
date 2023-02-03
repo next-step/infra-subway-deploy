3e04a582ab002e87b8d0bc78968a5f5bf4576a50
kmus1232 <kmus1232@gmail.com>
2023-02-03 16:00:23 +0900

    웹서버 실행 스크립트 추가

---
+++ run_web_server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git pull
+git checkout step2
+./gradlew clean build
+
+nohup java -jar -Dspring.profiles.active=local ./build/libs/subway-0.0.1-SNAPSHOT.jar &
+
+#docker restart -d -p 80:80 -p 443:443 --name proxy nextstep/reverse-proxy:0.0.2
+docker restart proxy
+docker restart local_db
