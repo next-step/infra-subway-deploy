81279e2bda7ae2d3946bb837836c342cf5c95be9
kmus1232 <kmus1232@gmail.com>
2023-02-04 11:29:51 +0900

    prod db 사용하도록 스크립트 수정

--- run_web_server.sh
+++ run_web_server.sh
@@ -3,7 +3,7 @@ git pull
 git checkout step2
 ./gradlew clean build
 
-nohup java -jar -Dspring.profiles.active=local ./build/libs/subway-0.0.1-SNAPSHOT.jar &
+nohup java -jar -Dspring.profiles.active=prod ./build/libs/subway-0.0.1-SNAPSHOT.jar &
 
 #docker restart -d -p 80:80 -p 443:443 --name proxy nextstep/reverse-proxy:0.0.2
 docker restart proxy
