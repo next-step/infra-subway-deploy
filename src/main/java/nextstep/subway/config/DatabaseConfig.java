74125a029aa868f6608880c1a6672c8af5efe34f
ggam-nyang <v44ads@naver.com>
2023-02-07 16:32:09 +0900

    prod profile added

---
+++ src/main/java/nextstep/subway/config/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package nextstep.subway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
+
