68e60aa2ac63a3710006a6ad80ae832243eb1e8f
dev-taeyeong <dev.taeyeong@gmail.com>
2023-02-09 23:57:45 +0900

    그럴듯한 인프라 만들기 - 3단계

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ SHELL_SCRIPT_PATH=$(dirname $0)
 BRANCH=$1
 PROFILE=$2
 
+JAR_DIR="${SHELL_SCRIPT_PATH}/build/libs"
+
 txtrst='\033[1;37m' # White
 txtred='\033[1;31m' # Red
 txtylw='\033[1;33m' # Yellow
@@ -24,20 +26,42 @@ fi
 function pull() {
         echo -e ""
         echo -e ">> Pull Request 🏃♂️ "
+        git pull origin $BRANCH
 }
-pull;
 
+function kill_app() {
+  echo -e ">> kill running application"
+  currPid=$(pgrep -f "java -jar")
+  if [ -z $currPid ]; then
+    echo "No running application"
+  else
+    kill -2 $currPid
+    sleep 5
+  fi
+}
+
+function start_app() {
+  echo -e ""
+  echo -e "Start application"
+  jarFile=$(ls -tr ./build/libs/ | grep jar | tail -n 1)
+  nohup java -jar -Dspring.profiles.active=$PROFILE "${JAR_DIR}/${jarFile}" 1> web.log 2>&1 &
+}
+
+pull;
+check_df;
+kill_app;
+start_app;
+
+# crontab에서 실행하는 스크립트
 function check_df() {
   git fetch
   master=$(git rev-parse $BRANCH)
   remote=$(git rev-parse origin/$BRANCH)
 
-  echo "$master"
-  echo "$remote"
-
   if [[ $master == $remote ]]; then
     echo -e "[$(date)] Nothing to do!!!  😫"
     exit 1
+  else
+    git merge origin/$BRANCH
   fi
 }
-check_df;
